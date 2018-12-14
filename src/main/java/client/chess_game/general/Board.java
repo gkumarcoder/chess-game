e434209012cdd6b7022666af6ee7cd8b7936a053
Gaurav Kumar <gkumar4@outlook.com>
2018-12-14 16:33:50 +0530

    Update Board.java

--- src/main/java/client/chess_game/general/Board.java
+++ src/main/java/client/chess_game/general/Board.java
@@ -1,32 +1,9 @@
-/*
-	Copyright (C) 2010 Petri Tuononen
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package client.chess_game.general;
 import java.util.*;
 
 import client.chess_game.tools.Index;
 
-/**
- * @author Petri Tuononen
- * @since 24/11/2009
- * notice: it's important to notice that x is equal to board row
- *         and y is equal to board column.
- * modifications 8th May 2010
- * -enabled initBoard() in constructor
- */
 public class Board {
 
 	//global variables
